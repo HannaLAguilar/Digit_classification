d87cecc442e55b2b53eed98dc42f8eb601d6e8e2
Hanna Lizarzaburu Aguilar <hanna.lizarzaburu@gmail.com>
2020-11-19 17:37:56 +0100

    training CNN

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -6,11 +7,18 @@ import cv2
 from PIL import Image
 
 import torch
-from torchvision import transforms, datasets
+from torchvision import transforms
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader, SubsetRandomSampler
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
 from utils import *
+
+##########
+#  DATA  #
+##########
 
 # Basic parameters
 data_path = 'data/'
@@ -21,8 +29,8 @@ batch_size = 128
 # Dataset
 transform = transforms.Compose([transforms.Resize([32, 32]),
                                 transforms.Grayscale(),
-                                transforms.RandomHorizontalFlip(),  # data augmentation
-                                transforms.RandomRotation(10),  # data augmentation
+                                # transforms.RandomHorizontalFlip(),  # data augmentation
+                                # transforms.RandomRotation(10),  # data augmentation
                                 transforms.ToTensor()])
 
 dataset = ImageFolder(data_path, transform=transform)
@@ -31,7 +39,7 @@ print('Total classes:', len(dataset.classes))
 
 # Visualize classes
 samples_by_class = num_class(dataset)
-# sns.barplot(list(samples_by_class.keys()), list(samples_by_class.values()))
+# sns.barplot(x=list(samples_by_class.keys()), y=list(samples_by_class.values()))
 
 # Split dataset
 train_idx, valid_idx, test_idx = train_valid_test(dataset, train_ratio=0.8, valid_ratio=0.2)
@@ -44,10 +52,70 @@ data_loaders = {
     'train': DataLoader(dataset, sampler=train_sampler, batch_size=batch_size),
     'valid': DataLoader(dataset, sampler=valid_sampler, batch_size=batch_size),
     'test': DataLoader(dataset, sampler=test_sampler, batch_size=batch_size)
-            }
+}
 
 # Visualize img example
 images, labels = iter(data_loaders['train']).next()
 images = images.numpy()
 img = images[0].squeeze()
 plt.figure(), plt.imshow(img, cmap='gray')
+
+
+#############
+# CNN MODEL #
+#############
+
+# CNN architecture
+class Classifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 16, 5, padding=2)
+        self.conv2 = nn.Conv2d(16, 32, 5, padding=2)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(8 * 8 * 32, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        return x
+
+
+model = Classifier()
+print(model)
+
+######################
+# TRAINING THE MODEL #
+######################
+
+# CPU/GPU settings
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print('Training on {}'.format(device.type))
+model = model.to(device)
+
+# Training
+epochs = 5
+learning_rate = 0.01
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
+train_loss, valid_loss, valid_acc = train_model(epochs, model, data_loaders, criterion, optimizer, device)
+
+plt.subplot(121)
+plt.plot(train_loss, label='Train loss')
+plt.plot(valid_loss, label='Valid loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.subplot(122)
+plt.plot(valid_acc, label='Valid Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+
+# images, labels = iter(data_loaders['train']).next()
+# img = images[0].unsqueeze(0)
+# img = img.to(device)
+# output = model(img)
+# _, pred = torch.max(output, 1)
--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
+import time
 import numpy as np
+import matplotlib.pyplot as plt
+import torch
 
 
 def num_class(dataset):
@@ -18,9 +21,78 @@ def train_valid_test(dataset, train_ratio, valid_ratio):
     valid_num = int(valid_ratio * dataset_num)
     indices = list(range(dataset_num))
     np.random.shuffle(indices)
-    return indices[:train_num], indices[train_num: train_num+valid_num], indices[train_num+valid_num:]
+    return indices[:train_num], indices[train_num: train_num + valid_num], indices[train_num + valid_num:]
 
 
+def train_model(epochs, model, data_loaders, criterion, optimizer, device):
+    since = time.time()
+    best_acc = 0
+
+    train_loss_history, valid_loss_history = [], []
+    valid_accuracy_history = []
+    for epoch in range(epochs):
+        train_loss, valid_loss = 0, 0
+        print('Epoch: {}/{}'.format(epoch + 1, epochs))
+        print('-' * 10)
+
+        # Train
+        model.train()
+        for images, labels in data_loaders['train']:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            optimizer.zero_grad()
+            output = model(images)
+            loss = criterion(output, labels)
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item() * images.size(0)
+
+        # Valid
+        model.eval()
+        valid_corrects = 0
+        accuracy = None
+        for images, labels in data_loaders['valid']:
+            images = images.to(device)
+            labels = labels.to(device)
+            output = model(images)
+            loss = criterion(output, labels)
+            valid_loss += loss.item() * images.size(0)
+
+            _, preds = torch.max(output, 1)
+            valid_corrects += (preds == labels).sum().item()
+            accuracy = valid_corrects / len(data_loaders['valid'].sampler)
+
+        # calculate average losses
+        train_loss = train_loss / len(data_loaders['train'].sampler)
+        valid_loss = valid_loss / len(data_loaders['valid'].sampler)
+
+        # history average losses
+        train_loss_history.append(train_loss)
+        valid_loss_history.append(valid_loss)
+
+        # valid accuracy history
+        valid_accuracy_history.append(accuracy)
+
+        print('Train loss:{:.3}  Valid loss:{:.3}  Valid acc:{:.1%}'.format(train_loss, valid_loss, accuracy))
+        print()
+
+        # save model
+        if accuracy > best_acc:
+            best_acc = accuracy
+            torch.save(model.state_dict(), 'model_digit.pt')
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best Valid Acc: {:.1%}'.format(best_acc))
+
+    # return {'train_loss': train_loss_history,
+    #         'valid_loss': valid_loss_history,
+    #         'valid_acc': valid_accuracy_history}
+    return train_loss_history, valid_loss_history, valid_accuracy_history
 
 
-
+# def loss_plot(**kwargs):
+#     for key, value in kwargs.items():
+#         plt.plot(value, label=key)
+#         plt.legend()
